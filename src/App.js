d621ee4b8ec41ccde79afb5a75683f38de895a96
suraj <surajadhikarycs@gmail.com>
2022-04-09 05:46:42 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import * as React from 'react';
+// import { DocumentEditorContainerComponent, Toolbar } from '@syncfusion/ej2-react-documenteditor';
+// DocumentEditorContainerComponent.Inject(Toolbar);
+// export class App extends React.Component {
+//     render() {
+//         return (<DocumentEditorContainerComponent id="container" height={'100vh'} serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/" enableToolbar={true}/>);
+//     }
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+// export default App
+
+/**
+ * Rich Text Editor - Insert Image Sample
+ */
+ import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+ import * as React from 'react';
+ import './App.css';
+ class App extends React.Component {
+     constructor() {
+         super(...arguments);
+         this.toolbarSettings = {
+             items: ['Bold', 'Italic', 'Underline', 'StrikeThrough',
+                 'FontName', 'FontSize', 'FontColor', 'BackgroundColor',
+                 'LowerCase', 'UpperCase', '|',
+                 'Formats', 'Alignments', 'OrderedList', 'UnorderedList',
+                 'Outdent', 'Indent', '|',
+                 'CreateLink', 'Image', '|', 'ClearFormat', 'Print',
+                 'SourceCode', 'FullScreen', '|', 'Undo', 'Redo']
+         };
+         this.quickToolbarSettings = {
+             image: ['Replace', 'Align', 'Caption', 'Remove', 'InsertLink', 'OpenImageLink', '-', 'EditImageLink', 'RemoveImageLink', 'Display', 'AltText', 'Dimension']
+         };
+     }
+     render() {
+        return (
+        <RichTextEditorComponent 
+            height={450} 
+            toolbarSettings={this.toolbarSettings} 
+            quickToolbarSettings={this.quickToolbarSettings}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+            <p>The Rich Text Editor component is WYSIWYG ("what you see is what you get") editor that provides the best user experience to create and update the content.
+                Users can format their content using standard toolbar commands.</p>
+            <p><b>Key features:</b></p>
+            <ul>
+                {/* <li>
+                <p>Provides <IFRAME> and <DIV> modes</p>
+                </li> */}
+                <li>
+                <p>Capable of handling markdown editing.</p>
+                </li>
+                <li>
+                <p>Contains a modular library to load the necessary functionality on demand.</p>
+                </li>
+                <li>
+                <p>Provides a fully customizable toolbar.</p>
+                </li>
+                <li>
+                <p>Provides HTML view to edit the source directly for developers.</p>
+                </li>
+                <li>
+                <p>Supports third-party library integration.</p>
+                </li>
+                <li>
+                <p>Allows preview of modified content before saving it.</p>
+                </li>
+                <li>
+                <p>Handles images, hyperlinks, video, hyperlinks, uploads, etc.</p>
+                </li>
+                <li>
+                <p>Contains undo/redo manager.</p>
+                </li>
+                <li>
+                <p>Creates bulleted and numbered lists.</p>
+                </li>
+            </ul>
+            <Inject services={[Toolbar, Image, Link, HtmlEditor, QuickToolbar]}/>
+        </RichTextEditorComponent>);
+    }
+ }
+ export default App;
